c6dd94d053f9827ca07f01844afd3bb37670cfb8
keerthibalan9 <keerthiibalan9@gmail.com>
2023-09-10 14:56:49 +0530

    just normal addition

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var name="keerthibalan"
-document.write("My name is " +name)
+var a=parseInt(prompt("Enter number 1"));
+
+var b=parseInt(prompt("Enter number 2"));
+
+
+var c=parseInt(prompt("Enter number 3"));
+
+var d=parseInt(prompt("Enter number 4"));
+
+var total=a+b+c+d
+document.write('Total of 4 numbers are',+""+ total);
